c34afb3d8a6f61a95b59c4960f320a0b0a759f97
Rohith Sagar <rohithsagar14325@gmail.com>
2025-11-13 20:11:01 +0400

    Fully remove all react-slick and react-parallax-scroll references

--- src/components/about-us/marquee/page.jsx
+++ src/components/about-us/marquee/page.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-// import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+
+
 import { Container } from 'react-bootstrap';
 
 import Image1 from '../../../assets/images/about/1.jpg';
